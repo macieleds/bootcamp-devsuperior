2b31be3ab45828da0b7ce5dda24b7ae7b459d89c
Edison Maciel <maciel.eds@gmail.com>
2021-07-04 19:43:07 -0300

    Fex: PostgreSQL compatibility

--- backend/src/main/java/com/edisonmaciel/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/edisonmaciel/dscatalog/repositories/ProductRepository.java
@@ -8,11 +8,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query("SELECT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
-            + ":category IN cats")
-    Page<Product> find(Category category, Pageable pageable);
-
+    @Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
+            + "(COALESCE (:categories) IS NULL OR cats IN :categories) AND "
+            + "(LOWER(obj.name) LIKE LOWER(CONCAT('%',:name,'%'))) ")
+    Page<Product> find(List<Category> categories, String name, Pageable pageable);
 }
