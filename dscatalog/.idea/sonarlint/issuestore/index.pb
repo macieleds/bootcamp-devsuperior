b75503065649494dbc233525b7be319caa98183b
Edison Maciel <maciel.eds@gmail.com>
2021-07-10 19:33:26 -0300

    Deploy to prod environment

---
+++ dscatalog/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+†
+Vbackend/src/main/java/com/edisonmaciel/dscatalog/config/AuthorizationServerConfig.java,c/1/c199a497b6e2b34ddff9c7931bf91e39a1b02577
+S
+#backend/src/main/resources/data.sql,6/0/6021c521dfc9aec020b28dade4b45f00fff2eabb
+s
+Cbackend/src/main/java/com/edisonmaciel/dscatalog/entities/Role.java,b/f/bf5bbc29dd06e4d3c1b12dc2a78dcf1579a60ed4
+v
+Fbackend/src/main/java/com/edisonmaciel/dscatalog/entities/Product.java,7/8/7884b679629dbeecd75906137b596d75a24914b4
+w
+Gbackend/src/main/java/com/edisonmaciel/dscatalog/entities/Category.java,f/2/f2998b093c29a4beb7f4fdfd34df0aff135784a7
+s
+Cbackend/src/main/java/com/edisonmaciel/dscatalog/entities/User.java,a/a/aacf5dcf39eef1736832b23913bc94fa5dcaf70d
+a
+1backend/src/main/resources/application.properties,3/f/3f2f17daa13c76ba02d9d8cf5498cdfd7ebe2ce8
+z
+Jbackend/src/main/java/com/edisonmaciel/dscatalog/DscatalogApplication.java,1/3/13b69f86f6dfd3642674fb7d983b8937d6b311df
---
+++ dscatalog/backend/src/main/java/com/edisonmaciel/dscatalog/services/validation/UserUpdateValidator.java
@@ -0,0 +1,51 @@
+package com.edisonmaciel.dscatalog.services.validation;
+
+import com.edisonmaciel.dscatalog.dto.UserInsertDTO;
+import com.edisonmaciel.dscatalog.dto.UserUpdateDTO;
+import com.edisonmaciel.dscatalog.entities.User;
+import com.edisonmaciel.dscatalog.repositories.UserRepository;
+import com.edisonmaciel.dscatalog.resources.exceptions.FieldMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserUpdateDTO> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Override
+    public void initialize(UserUpdateValid ann) {
+    }
+
+    @Override
+    public boolean isValid(UserUpdateDTO dto, ConstraintValidatorContext context) {
+
+        @SuppressWarnings("unchecked")
+        var uriVars = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        long userId = Long.parseLong(uriVars.get("id"));
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        User user = userRepository.findByEmail(dto.getEmail());
+        if (user != null && userId != user.getId()) {
+            list.add(new FieldMessage("email", "Email já existente."));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
